8be275720bdfa63425ce67c35b52a31e7f56dcfb
JoaoOFS <71888050+JoaoOFS@users.noreply.github.com>
2021-01-22 02:14:13 -0300

    Add files via upload

---
+++ JogoPerguntas/js/deck.js
@@ -0,0 +1,122 @@
+var deck = [
+    {
+        id: 0,
+        id1: "q1alt1",
+        id2: "q1alt2",
+        id3: "q1alt3",
+        nameId: "q1",
+        titulo: "Quais sãos os principais personagens do enredo GTA 5?",
+        imgSrc : "./img/personagens.jpg",
+        alternativa1 : "Franklin, Lester e Michael",
+        alternativa2 : "Carl, Big Smoke e Sweet",
+        alternativa3 : "Michael, Franklin e Trevor",
+        correta : 3,
+        pontos: 200
+    },
+    {
+        id: 1,
+        id1: "q2alt1",
+        id2: "q2alt2",
+        id3: "q2alt3",
+        nameId: "q2",
+        titulo: "Em que ano o jogo foi lançado ?",
+        imgSrc : "./img/data.jpg",
+        alternativa1 : "15 de Novembro de 2013",
+        alternativa2 : "17 de Setembro de 2013",
+        alternativa3 : "21 de Março de 2014",
+        correta: 2,
+        pontos: 200
+    },
+    {
+        id: 2,
+        id1: "q3alt1",
+        id2: "q3alt2",
+        id3: "q3alt3",
+        nameId: "q3",
+        titulo: "Qual nome da esposa de Michael ?",
+        imgSrc : "./img/esposa.png",
+        alternativa1 : "Amanda",
+        alternativa2 : "Patricia",
+        alternativa3 : "Angela",
+        correta: 1,
+        pontos : 100
+    },
+
+    {
+        id: 3,
+        id1: "q4alt1",
+        id2: "q4alt2",
+        id3: "q4alt3",
+        nameId: "q4",
+        titulo: "Qual nome do cachorro do Franklin ?",
+        imgSrc : "./img/dog.jpg",
+        alternativa1 : "Chopp",
+        alternativa2 : "Rex",
+        alternativa3 : "Brutus",
+        correta: 1,
+        pontos : 100
+    },
+
+    {
+        id: 4,
+        id1: "q5alt1",
+        id2: "q5alt2",
+        id3: "q5alt3",
+        nameId: "q5",
+        titulo: "Qual nome do filho de Michael ?",
+        imgSrc : "./img/filho.jpeg",
+        alternativa1 : "Jimmy de Santa",
+        alternativa2 : "Jhon de Santa",
+        alternativa3 : "Jemmy de Santa",
+        correta: 1,
+        pontos : 100
+    },
+
+    {
+        id: 5,
+        id1: "q6alt1",
+        id2: "q6alt2",
+        id3: "q6alt3",
+        nameId: "q6",
+        titulo: "Qual nome desse personagem ?",
+        imgSrc : "./img/Lester.jpg",
+        alternativa1 : "Atila",
+        alternativa2 : "Lester",
+        alternativa3 : "Michael",
+        correta: 2,
+        pontos : 100
+    },
+
+    {
+        id: 6,
+        id1: "q7alt1",
+        id2: "q7alt2",
+        id3: "q7alt3",
+        nameId: "q7",
+        titulo: "Qual é o predio mais alto em Los Santos?",
+        imgSrc : "./img/predios.png",
+        alternativa1 : "Prédio LC|24",
+        alternativa2 : "Prédio do FIB",
+        alternativa3 : "Maze Bank",
+        correta: 3,
+        pontos : 100
+    },
+
+    {
+        id: 7,
+        id1: "q8alt1",
+        id2: "q8alt2",
+        id3: "q8alt3",
+        nameId: "q8",
+        titulo: "Qual é o personagem conhecido como louco ?",
+        imgSrc : "./img/personagens2.jpg",
+        alternativa1 : "Trevor",
+        alternativa2 : "Michael",
+        alternativa3 : "Franklin",
+        correta: 1,
+
+        pontos : 100
+    },
+
+]
+    
